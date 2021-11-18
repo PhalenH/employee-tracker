1270f7c8425e8d75473445b2d251ba02bdd35b59
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-11-18 17:41:20 -0500

    initialized files and folders including: db folder with schema/query/seeds files, package and package-lock.json, and a server.js file which I required necessary packages

---
+++ server.js
@@ -0,0 +1,5 @@
+// const express = require("express");
+// const fs = require("fs");
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
