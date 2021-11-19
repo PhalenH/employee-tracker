fb9bdf7f0b2ced2a60ab2168b221c9d199676c07
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-11-18 20:32:22 -0500

    in index.js: created connection to database, created 5 functions that use inquirer to ask user a question with questions from acceptance criteria, need to add functionality and .then statements for each function: updated readme

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# employee-tracker
-week 12 homework assignment
+# Team Profile Generator
+
+## Table of Contents
+- [Description](#Description)
+- [Video](#Video)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Questions](#Questions)
+
+## Description: 
+The purpose of this application 
+ <!-- TODO fill out description -->
+
+- WHEN I start the application
+    - THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+- WHEN I choose to view all departments
+    - THEN I am presented with a formatted table showing department names and department ids
+- WHEN I choose to view all roles
+    - THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+- WHEN I choose to view all employees
+    - THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+- WHEN I choose to add a department
+    - THEN I am prompted to enter the name of the department and that department is added to the database
+- WHEN I choose to add a role
+    - THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+- WHEN I choose to add an employee
+    - THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+- WHEN I choose to update an employee role
+    - THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
+
+## Video:
+[Employee Tracker Walkthrough Vdieo](####)
+<!-- add link to video when finished and uploaded -->
+
+## Installation: 
+- Download or clone the repo from GitHub
+
+## Usage: 
+Once downloaded
+- A user will open a new terminal
+- Type in "npm i" to install dependencies
+- In the same terminal or a new terminal: Type in "node index.js"
+- This will start the prompts
+- Select from the options presented
+<!-- add additional instructions as needed -->
+
+## Contributing: 
+  There a no plans for public contribution at this time.
+
+## Questions
+- If you have any questions, email me at pchaze@yahoo.com
+- Checkout my GitHub profile [here](https://github.com/PhalenH)
+
+
--- index.js
+++ index.js
@@ -3,3 +3,150 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
+
+const connection = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'rootpassword',
+      database: 'business_db'
+    },
+    console.log(`Connected to the business_db database.`)
+  );
+
+// WHEN I start the application
+// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+function initial() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do",
+        name: "initial",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+        ],
+      },
+    ])
+    .then((initialChoice) => {
+      // either if else statements or switch case here
+    });
+}
+
+// WHEN I choose to view all departments
+// THEN I am presented with a formatted table showing department names and department ids
+
+// WHEN I choose to view all roles
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+
+// WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the name of the department.",
+        name: "department",
+      },
+    ])
+    .then((departmentName) => {
+      // add to department database
+    });
+}
+
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the name of the role?",
+        name: "roleName",
+      },
+      {
+        type: "input",
+        message: "What is the salary of the role?",
+        name: "roleSalary",
+      },
+      {
+        type: "list",
+        message: "Which department does the role belong to?",
+        name: "roleDepartment",
+        choices: [], // figure out way to use the department db so this list in always up to date if user adds a department
+      },
+    ])
+    .then((roleData) => {
+      // add to role database
+    });
+}
+
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the employee’s first name?",
+        name: "firstName",
+      },
+      {
+        type: "input",
+        message: "What is the employee’s last name?",
+        name: "lastName",
+      },
+      {
+        type: "input",
+        message: "What is the employee’s role?",
+        name: "employeeRole",
+      },
+      {
+        type: "input",
+        message: "Who is the employee’s manager?",
+        name: "employeeManager",
+      },
+    ])
+    .then((employeeData) => {
+      // add to employee database
+    });
+}
+
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+function updateRole() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "Which employee's role do you want to update?",
+          name: "selectedEmployee",
+          choices: [], // array of employee names from db
+        },
+        {
+            type: "list",
+            message: "Which role do you want to assign to the selected employee?",
+            name: "newRole",
+            choices: [], // array of roles  from db
+          },
+      ])
+      .then((updatedData) => {
+        // update database for role and employee
+      });
+  }
+
+
+
+// initial();
